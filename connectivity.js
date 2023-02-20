f36c861f09d954ac7a61d5c928c4d838cd6b7688
Amias <32610164+AmiasWayne@users.noreply.github.com>
2023-02-20 09:02:23 +0800

    Update connectivity.js

--- connectivity.js
+++ connectivity.js
@@ -1,31 +1,28 @@
 let $ = {
-  Bilibili: 'https://www.bilibili.com',
-  Baidu: 'https://www.baidu.com',
-  Youtube: 'https://www.youtube.com/',
-  Google: 'https://www.google.com/generate_204',
-  Github: 'https://www.github.com'
+Bilibili:'https://www.bilibili.com',
+Baidu:'https://www.baidu.com',
+Google:'https://www.google.com/generate_204',
+Github:'https://www.github.com'
 }
 
 !(async () => {
-  let test = Object.keys($).map((key) => [Number(key), obj[key]])
-  await Promise.all(test).then((x) => {
-    $done({
-      title: 'Network Connectivity Test',
-      content: x.join('\n'),
-      icon: 'timer',
-      'icon-color': '#FF5A9AF9',
-    })
+await Promise.all([http('Baidu'),http('Bilibili'),http('Github'),http('Google')]).then((x)=>{
+	$done({
+    title: 'Network Connectivity Test',
+    content: x.join('\n'),
+    icon: 'timer',
+    'icon-color': '#FF5A9AF9',
   })
+})
 })();
 
 function http(req) {
-  return new Promise((r) => {
-    let _req = req[1]
-    let time = Date.now();
-    $httpClient.post(_req, (err, resp, data) => {
-      r(req[0] +
-        '\xa0\xa0\xa0\xa0\xa0\t: ' +
-        (Date.now() - time) + ' ms');
+    return new Promise((r) => {
+			let time = Date.now();
+        $httpClient.post($[req], (err, resp, data) => {
+            r(req +
+						'\xa0\xa0\xa0\xa0\xa0\t: ' +
+						(Date.now() - time)+' ms');
+        });
     });
-  });
 }
