3a051980cff9292fa33b42823ee2ef945440381b
Amias <32610164+AmiasWayne@users.noreply.github.com>
2023-02-21 09:05:38 +0800

    更新 latency.js

--- latency.js
+++ latency.js
@@ -26,7 +26,7 @@ async function test_baidu() {
   let inner_check = () => {
     return new Promise((resolve) => {
       let option = {
-        url: 'http://www.baidu.com',
+        url: 'https://www.baidu.com',
         headers: REQUEST_HEADERS,
       }
       baidu_startTime = Date.now()
